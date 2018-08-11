234f9d1e65f9911e06a95cbd432ba12242c2fcf4
Jacques Uber <uberj@miradorfin.com>
2018-08-11 16:54:21 -0700

    [master] graph works

--- spacetime.py
+++ spacetime.py
@@ -12,7 +12,21 @@ class SpaceTime(object):
         for e in self.edges:
             G.add_node(e.s1)
             G.add_node(e.s2)
-            G.add_edge(e.s1, e.s2, weight=e.time_between, object={'time_between': e.time_between})
-        labels = nx.get_edge_attributes(G,'time_between')
-        nx.draw(G, with_labels=True, edge_lables=labels)
+            G.add_edge(
+                    e.s1,
+                    e.s2,
+                    weight=e.time_between,
+                    object={'time_between': e.time_between})
+        pos=nx.spring_layout(G)
+        edge_labels = {
+                (edge[0], edge[1]):
+                edge[2]['object'] for edge in
+                G.edges(data=True)
+        }
+
+        edge_labels=dict([((u,v,),d['weight']) for u,v,d in G.edges(data=True)])
+        nx.draw_networkx_edge_labels(
+                G,pos,
+                edge_labels=edge_labels)
+        nx.draw(G, with_labels=True)
         plt.show()
